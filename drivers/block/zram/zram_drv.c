2e215193a06d2e96fb149023177e6f70e2770f96
Jianyue Wu <wujianyue000@gmail.com>
2026-06-07 19:58:15 +0800

    mm/swap: route slot free notifications through swap_ops
    Dispatch slot_free_notify through swap_ops instead of
    block_device_operations. Zram keeps slot-free handling alongside its
    other swap_ops methods.
    
    Document the callback locking rules in include/linux/swap.h. Remove
    the outdated locking.rst note for swap_slot_free_notify.
    
    Signed-off-by: Jianyue Wu <wujianyue000@gmail.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -2797,10 +2797,11 @@ static void zram_submit_bio(struct bio *bio)
 	}
 }
 
-static void zram_slot_free_notify(struct block_device *bdev,
-				  unsigned long index)
+#if IS_ENABLED(CONFIG_SWAP)
+static void zram_swap_slot_free_notify(struct swap_info_struct *sis,
+				       unsigned long index)
 {
-	struct zram *zram = bdev->bd_disk->private_data;
+	struct zram *zram = sis->bdev->bd_disk->private_data;
 
 	atomic64_inc(&zram->stats.notify_free);
 	if (!slot_trylock(zram, index)) {
@@ -2811,6 +2812,7 @@ static void zram_slot_free_notify(struct block_device *bdev,
 	slot_free(zram, index);
 	slot_unlock(zram, index);
 }
+#endif /* CONFIG_SWAP */
 
 static void zram_comp_params_reset(struct zram *zram)
 {
@@ -3012,6 +3014,7 @@ static const struct swap_ops zram_swap_ops = {
 	.can_merge		= swap_bdev_can_merge,
 	.submit_read		= swap_bdev_submit_read,
 	.submit_write		= zram_swap_submit_write,
+	.slot_free_notify	= zram_swap_slot_free_notify,
 };
 
 #endif /* CONFIG_SWAP */
@@ -3019,7 +3022,6 @@ static const struct swap_ops zram_swap_ops = {
 static const struct block_device_operations zram_devops = {
 	.open = zram_open,
 	.submit_bio = zram_submit_bio,
-	.swap_slot_free_notify = zram_slot_free_notify,
 	.owner = THIS_MODULE
 };
 
